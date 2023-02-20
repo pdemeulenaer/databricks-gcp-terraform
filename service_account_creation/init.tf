a9eba59ec11a5af7fa8aa4b5452f4b00e7839dd3
pdemeulenaer <pdemeulenaer@gmail.com>
2023-02-20 13:56:40 +0200

    set up the backend for the state

--- service_account_creation/init.tf
+++ service_account_creation/init.tf
@@ -1,4 +1,10 @@
 terraform {
+	backend "remote" {
+		organization = "example-org-d50e3c" # org name in Terraform Cloud
+		workspaces {
+			name = "databricks-gcp-terraform-sa" # name of the workspace containing the state file
+		}
+	}   
   required_providers {
     google = {
       source = "hashicorp/google"
