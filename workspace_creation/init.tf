1fde85b6f6030dd2c784c1fe468fb70294473788
pdemeulenaer <pdemeulenaer@gmail.com>
2023-02-20 14:26:50 +0200

    add workspace configuration

---
+++ workspace_creation/init.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    databricks = {
+      source = "databricks/databricks"
+      version = "1.9.2"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "4.53.1"
+    }
+  }
+}
+
+provider "google" {
+  project = var.google_project
+  region  = var.google_region
+  zone    = var.google_zone
+}
+
+# Provider defined at Databricks Account level
+provider "databricks" {
+  alias                  = "accounts"
+  host                   = "https://accounts.gcp.databricks.com"
+  google_service_account = var.databricks_google_service_account
+  account_id             = var.databricks_account_id
+}
+
+data "google_client_openid_userinfo" "me" {
+}
+
+data "google_client_config" "current" {
+}
+
+resource "random_string" "suffix" {
+  special = false
+  upper   = false
+  length  = 6
+}
---
+++ workspace_creation/variables.tf
@@ -0,0 +1,5 @@
+variable "databricks_account_id" {}
+variable "databricks_google_service_account" {}
+variable "google_project" {}
+variable "google_region" {}
+variable "google_zone" {}
